83c9f90773b7b3f712a76d2996411e670720d239
nabeelahmed99 <62593855+nabeelahmed1699@users.noreply.github.com>
2024-09-23 12:36:02 +0500

    skeleton loading tiles

--- src/components/movies/index.tsx
+++ src/components/movies/index.tsx
@@ -8,7 +8,17 @@ const Movies = () => {
   const { data, isPending } = useMovies();
 
   if (isPending) {
-    return 'Loading...';
+    return (
+      <div className='flex flex-col gap-2'>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((row) => (
+          <div key={row} className='grid grid-cols-6 gap-1'>
+            {[1, 2, 3, 4, 5, 6].map((item) => {
+              return <div key={item} className='h-40 rounded-md aspect-video w-full bg-gray-500 animate-pulse'></div>;
+            })}
+          </div>
+        ))}
+      </div>
+    );
   }
 
   return (
